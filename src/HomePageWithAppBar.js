fc7f9573276f20902e37123a676f87753ee62d97
AGS Lakpahana <agslakpahana@gmail.com>
2023-09-03 21:34:10 +0530

    add a grid

--- src/HomePageWithAppBar.js
+++ src/HomePageWithAppBar.js
@@ -19,142 +19,98 @@ const Home2 = () => {
 
             <Container maxWidth="md">
                 <Toolbar
-                    
+
                 >
-                  
-                    <Box
+                    <Grid container
                         sx={{
-                            gap: 1,
-                            display: "flex",
-                            flexDirection: "row"
+                            alignItems: "center",
+
                         }}
                     >
-                        <Book
-                            sx={{
-                                fontSize: '200'
-                            }}
-                        ></Book>
-                        <Typography>Hello</Typography>
-                    </Box>
-                    <Box sx={{
-                        ml: 20,
-                        display: 'flex',
-                        alignItems: 'flex-end',
-                        bgcolor: "lightgray",
-                        borderRadius: 50,
-                        px: 1,
-                        py: 0.3,
-
-                    }}>
-                        <SearchOutlinedIcon sx={{ color: 'action.active', mx: 1, my: 0.5 }} />
-                        <TextField variant="standard"
-                            placeholder="Search"
-
-                            InputProps={{ disableUnderline: true }}
-                        />
-                    </Box>
-
-
-
-                    <Box
-                        sx={{
-                            display: "flex",
-                            justifyContent: "center",
-                            gap: 2,
-                            mt: 5,
-                            mb: 5,
-                            ml: 20,
-                        }}
-                    >
-                        
-                        <FacebookRoundedIcon
-                            sx={{
-                                fontSize: "180",
-                                color: "#fff",
-                                "&:hover": {
-                                    color: "#ffa902",
-                                },
-                                //round
-                                borderRadius: "50%",
-                            }}
-                        ></FacebookRoundedIcon>
-                        <TwitterIcon></TwitterIcon>
-                        <InstagramIcon></InstagramIcon>
-                        <YouTubeIcon></YouTubeIcon>
-
-
-                    </Box>
-                </Toolbar>
-                {/* <CssBaseline /> */}
-                <Divider variant="light" color="white"></Divider>           
-                <Grid container>
                         {/* three equal columns */}
                         <Grid item xs={4}>
-                        <Box
-                        sx={{
-                            gap: 1,
-                            display: "flex",
-                            flexDirection: "row"
-                        }}
-                    >
-                        <Book
-                            sx={{
-                                fontSize: '200'
-                            }}
-                        ></Book>
-                        <Typography>Hello</Typography>
-                    </Box>
-                            </Grid>
+                            <Box
+                                sx={{
+                                    gap: 1,
+                                    display: "flex",
+                                    flexDirection: "row"
+                                }}
+                            >
+                                {/* <Book
+                                    sx={{
+                                        fontSize: '200'
+                                    }}
+                                ></Book> */}
+                                <Typography
+                                    fontFamily={"Poppins, sans-serif"}
+                                    fontWeight={"700"}
+                                    fontSize={"2rem"}
+                                    sx={{
+                                        color: "#ffa902"
+                                    }}
+                                >Galli</Typography>
+                                <Typography
+                                    sx={{
+                                        ml: -1.2,
+                                    }}
+                                    fontFamily={"Poppins, sans-serif"}
+                                    fontWeight={"700"}
+                                    fontSize={"2rem"}
+                                >vant</Typography>
+                            </Box>
+                        </Grid>
                         <Grid item xs={4}>
-                        <Box sx={{
-                        ml: 20,
-                        display: 'flex',
-                        alignItems: 'flex-end',
-                        bgcolor: "lightgray",
-                        borderRadius: 50,
-                        px: 1,
-                        py: 0.3,
+                            <Box sx={{
+                                // ml: 20,
+                                width: '100%',
+                                display: 'flex',
+                                alignItems: 'flex-end',
+                                bgcolor: "lightgray",
+                                borderRadius: 50,
+                                px: 1,
+                                py: 0.3,
 
-                    }}>
-                        <SearchOutlinedIcon sx={{ color: 'action.active', mx: 1, my: 0.5 }} />
-                        <TextField variant="standard"
-                            placeholder="Search"
+                            }}>
+                                <SearchOutlinedIcon sx={{ color: 'action.active', mx: 1, my: 0.5 }} />
+                                <TextField variant="standard"
+                                    placeholder="Search"
 
-                            InputProps={{ disableUnderline: true }}
-                        />
-                    </Box>
-                            </Grid>
+                                    InputProps={{ disableUnderline: true }}
+                                />
+                            </Box>
+                        </Grid>
                         <Grid item xs={4}>
-                        <Box
-                        sx={{
-                            display: "flex",
-                            justifyContent: "center",
-                            gap: 2,
-                            mt: 5,
-                            mb: 5,
-                            ml: 20,
-                        }}
-                    >
-                        
-                        <FacebookRoundedIcon
-                            sx={{
-                                fontSize: "180",
-                                color: "#fff",
-                                "&:hover": {
-                                    color: "#ffa902",
-                                },
-                                //round
-                                borderRadius: "50%",
-                            }}
-                        ></FacebookRoundedIcon>
-                        <TwitterIcon></TwitterIcon>
-                        <InstagramIcon></InstagramIcon>
-                        <YouTubeIcon></YouTubeIcon>
+                            <Box
+                                sx={{
+                                    display: "flex",
+                                    justifyContent: "flex-end",
+                                    gap: 2
+                                }}
+                            >
+
+                                <FacebookRoundedIcon
+                                    sx={{
+                                        fontSize: "180",
+                                        color: "#fff",
+                                        "&:hover": {
+                                            color: "#ffa902",
+                                        },
+                                        //round
+                                        borderRadius: "50%",
+                                    }}
+                                ></FacebookRoundedIcon>
+                                <TwitterIcon></TwitterIcon>
+                                <InstagramIcon></InstagramIcon>
+                                <YouTubeIcon></YouTubeIcon>
 
 
-                    </Box>
-                            </Grid>
-                    </Grid>     
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </Toolbar>
+                {/* <CssBaseline /> */}
+                <Divider variant="light" color="white"></Divider>
+
             </Container>
 
             <Box
@@ -162,14 +118,28 @@ const Home2 = () => {
                     height: "500px",
                     position: "relative",
                     display: "flex",
+                    flexDirection: "column",
                     justifyContent: "center",
                     alignItems: "center",
                     color: "#fff",
                     fontSize: "4rem",
 
+
                 }}
             >
-                sadsa
+                <Typography
+                    fontFamily={"Poppins, sans-serif"}
+                    fontWeight={"800"}
+                    fontSize={"4rem"}
+                >
+                    Where will you go next ?
+                </Typography>
+                <Typography>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+                </Typography>
+                <Typography>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+                </Typography>   
             </Box>
         </AppBar>
 
